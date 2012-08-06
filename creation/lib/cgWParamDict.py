glideinWMS/glideinwms

Allow limits to be specified for different security classes of same frontend. Issue #2757 KNOWN ISSUE: Now format for name in <max_job_frontend name="" ..> is frontend:security_class. Admin needs to update the config to use name in the above format for limits to work correctly. There is no security_class attribute and this breaks backward compatibility. Admin needs to delete the security_class attribute of the config in work dir and config dir for reconfig to work.

creation/lib/cgWParamDict.py
@@ -688,12 +688,13 @@ def populate_job_descript(work_dir, job_descript_dict,
     max_held_frontends=""
     max_idle_frontends=""
     max_running_frontends=""
-    for X in sub_params.config.max_jobs.max_job_frontends.keys():
-        el=sub_params.config.max_jobs.max_job_frontends[X]
-        frontend_name=X+":"+el.security_class+";"
-        max_held_frontends += frontend_name+el.held+","
-        max_idle_frontends += frontend_name+el.idle+","
-        max_running_frontends += frontend_name+el.running+","
+    for frontend_name in sub_params.config.max_jobs.max_job_frontends.keys():
+        el=sub_params.config.max_jobs.max_job_frontends[frontend_name]
+        #frontend_name=X+":"+el.security_class+";"
+        #frontend_name = X
+        max_held_frontends += frontend_name+";"+el.held+","
+        max_idle_frontends += frontend_name+";"+el.idle+","
+        max_running_frontends += frontend_name+";"+el.running+","
     job_descript_dict.add("MaxRunningFrontends",max_running_frontends[:-1]);
     job_descript_dict.add("MaxHeldFrontends",max_held_frontends[:-1]);
     job_descript_dict.add("MaxIdleFrontends",max_idle_frontends[:-1]);

holzman/glideinwms-old

Conflicts:
	creation/lib/cgWParamDict.py
	creation/lib/cgWParams.py
	factory/glideFactoryLib.py

creation/lib/cgWParamDict.py
@@ -727,12 +727,11 @@ def populate_job_descript(work_dir, job_descript_dict,
     max_held_frontend = ""
     max_idle_frontend = ""
     max_glideins_frontend = ""
-    for X in sub_params.config.max_jobs.per_frontends.keys():
-        el = sub_params.config.max_jobs.per_frontends[X]
-        frontend_name = X + ":" + el.security_class + ";"
-        max_held_frontend += frontend_name + el.held + ","
-        max_idle_frontend += frontend_name + el.idle + ","
-        max_glideins_frontend += frontend_name + el.glideins + ","
+    for frontend_name in sub_params.config.max_jobs.per_frontends.keys():
+        el = sub_params.config.max_jobs.per_frontends[frontend_name]
+        max_held_frontend += frontend_name + ";" + el.held + ","
+        max_idle_frontend += frontend_name + ";" + el.idle + ","
+        max_glideins_frontend += frontend_name + ";" + el.glideins + ","
     job_descript_dict.add("PerFrontendMaxGlideins", max_glideins_frontend[:-1])
     job_descript_dict.add("PerFrontendMaxHeld", max_held_frontend[:-1])
     job_descript_dict.add("PerFrontendMaxIdle", max_idle_frontend[:-1])