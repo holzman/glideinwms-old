glideinWMS/glideinwms

Bug 2631: frontend downtime affecting other frontends
Currently, the code sets the entry into downtime
once it finds a security class / frontend into
downtime.  Then, later frontends will not be
able to send glideins (reqidle is set to zero somewhere)

This exactly matches Igor's description of the problem.

I have corrected this issue, but this should still be
investigated to make sure nothing else should be changed
as a result.

factory/glideFactoryEntry.py
@@ -470,8 +470,13 @@ def find_and_perform_work(in_downtime, glideinDescript, frontendDescript, jobDes
 
             if x509_proxies.count_fnames<1:
                 if security_class_downtime_found:
-                    glideFactoryLib.log_files.logWarning("Found proxies for client %s but the security class was in downtime, setting entry into downtime for advertising" % client_int_name)
-                    in_downtime = True
+                    glideFactoryLib.log_files.logWarning("Found proxies for client %s but the security class was in downtime" % client_int_name)
+                    #Note: This used to set the entry into downtime.
+                    #      That is a mistake since it is only in downtime
+                    #       for that security class/frontend.
+                    #Future work: notify the frontend somehow though? 
+                    #in_downtime = True
+                    continue
                 else:
                     glideFactoryLib.log_files.logWarning("No good proxies for %s, skipping request"%client_int_name)
                     continue #skip request

holzman/glideinwms-old

Conflicts:
	factory/glideFactoryEntry.py

factory/glideFactoryEntry.py
@@ -537,8 +537,13 @@ def find_and_perform_work(in_downtime, glideinDescript, frontendDescript, jobDes
 
             if x509_proxies.count_fnames < 1:
                 if security_class_downtime_found:
-                    logSupport.log.warning("Found proxies for client %s but the security class was in downtime, setting entry into downtime for advertising" % client_int_name)
-                    in_downtime = True
+                    logSupport.log.warning("Found proxies for client %s but the security class was in downtime" % client_int_name)
+                    #Note: This used to set the entry into downtime.
+                    #      That is a mistake since it is only in downtime
+                    #       for that security class/frontend.
+                    #Future work: notify the frontend somehow though? 
+                    #in_downtime = True
+                    continue
                 else:
                     logSupport.log.warning("No good proxies for %s, skipping request" % client_int_name)
                     continue