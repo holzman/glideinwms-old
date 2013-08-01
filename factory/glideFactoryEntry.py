glideinWMS/glideinwms

Move log directory cleanup into a separate process, so it does not slow down the main processing

factory/glideFactoryEntry.py
@@ -307,9 +307,21 @@ class Entry:
         # These two are used to write the history to disk
         self.gflFactoryConfig.qc_stats = glideFactoryMonitoring.condorQStats(logfiles=self.logFiles)
         self.gflFactoryConfig.client_internals = {}
+        self.logFiles.logActivity("Iteration initialized")
+
+
+    def doCleanup(self):
+        """
+        Perform the log file cleanup
+
+        """
+
+        self.loadContext()
 
         # Cleanup log files
+        self.logFiles.logActivity("Cleaning logs started")
         self.logFiles.cleanup()
+        self.logFiles.logActivity("Cleaning logs finished")
 
 
     def unsetInDowntime(self):

holzman/glideinwms-old

Conflicts:
	factory/glideFactoryEntry.py
	factory/glideFactoryEntryGroup.py

factory/glideFactoryEntry.py
@@ -336,6 +336,21 @@ class Entry:
         # These two are used to write the history to disk
         self.gflFactoryConfig.qc_stats = glideFactoryMonitoring.condorQStats(log=self.log)
         self.gflFactoryConfig.client_internals = {}
+        self.log.info("Iteration initialized")
+
+
+    def doCleanup(self):
+        """
+        Perform the log file cleanup
+
+        """
+
+        self.loadContext()
+
+        # Cleanup log files
+        self.log.info("Cleaning logs started")
+        self.log.cleanup()
+        self.log.info("Cleaning logs finished")
 
 
     def unsetInDowntime(self):